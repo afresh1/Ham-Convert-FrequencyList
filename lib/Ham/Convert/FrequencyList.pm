39fd3e6a7cced292fcc8bbade1489cf3c546bd74
Andrew Hewus Fresh <andrew@afresh1.com>
2021-04-24 11:52:27 -0700

    Add a few columns for testing
    It'll probably turn out that I need to define more in the "default" set,
    but for now we'll start simple and I can add them after I start looking
    at the actual files I have.

--- lib/Ham/Convert/FrequencyList.pm
+++ lib/Ham/Convert/FrequencyList.pm
@@ -15,6 +15,39 @@ sub new {
     return bless {%params}, $class;
 }
 
+sub headers {
+    my ($self) = @_;
+    return map { $_->{name} } $self->column_defs;
+}
+
+sub column_defs {
+    my @defs = map { { name => $_, internal => $_ } } qw<
+        id
+
+        tx_freq
+        duplex
+        rx_freq
+
+        ctcss
+        dtcsc
+        dtcsp
+
+        mode
+
+        name
+        comment
+    >;
+
+    push @defs, {
+        name     => 'groups',
+        internal => 'groups',
+        in       => sub { [ split /\s*:\s*/, $_ // '' ] },
+        out      => sub { join ':', @{ $_ || [] } },
+    };
+
+    return @defs;
+}
+
 1;
 __END__
 
@@ -41,6 +74,37 @@ ease that pain.
     my $converter = Ham::Convert::FrequencyList->new(
         ...,
     );
+
+=head2 headers
+
+Returns a list of the header names for the columns in the current format.
+
+=head1 INTERNALS
+
+These methods are used by subclasses to set up conversions.
+
+=head2 column_defs
+
+Returns a list of hashrefs defining the columns expected to be in
+the file.
+
+    my $defs = [ $converter->column_defs ];
+
+Now C<$defs> could look like:
+
+    [   {   name     => 'rx_freq',
+            internal => 'rx_freq',
+            in       => sub {...},
+            out      => sub {...},
+        },
+        {   name     => 'tx_freq',
+            internal => 'tx_freq',
+            in       => sub {...},
+            out      => sub {...},
+        },
+        ...,
+    ];
+
 
 =head1 BUGS AND LIMITATIONS
 
--- t/Ham-Convert-FrequencyList.t
+++ t/Ham-Convert-FrequencyList.t
@@ -1,10 +1,29 @@
 use Test2::V0 -target => 'Ham::Convert::FrequencyList',
-    qw< ok diag done_testing >;
+    qw< ok is diag done_testing >;
 
 diag "Testing $CLASS on perl $^V";
 
 ok CLASS, "Loaded $CLASS";
 
 ok my $converter = CLASS->new, "Created a new $CLASS instance";
+
+is [ $converter->headers ], [ qw<
+    id
+
+    tx_freq
+    duplex
+    rx_freq
+
+    ctcss
+    dtcsc
+    dtcsp
+
+    mode
+
+    name
+    comment
+
+    groups
+> ];
 
 done_testing;
